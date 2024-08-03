706fafdccd7bac0a9283befba9b3b2c887481bb0
Banno Taichi <taichib@icloud.com>
2024-08-04 00:24:04 +0900

    Add DB

--- create_db.py
+++ create_db.py
@@ -4,3 +4,7 @@ with app.app_context():
     db.create_all()
 
 # create_db.pyで作成されるデータベースtest.dbはinstanceフォルダ内に作られるが、それをGeekTwitter直下に置くと上手くいく
+# from app import app
+# app.app_contect().push()
+# from app import db
+# db.create_all()
